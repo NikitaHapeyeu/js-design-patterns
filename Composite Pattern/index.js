30c6c55546e08d1dca9ddd6f18d5c60e70343abd
Nikita Hapeyeu <Lard@tut.by>
2018-02-18 01:25:06 +0300

    implement FormComposite

--- Composite Pattern/index.js
+++ Composite Pattern/index.js
@@ -1,1 +1,30 @@
+var CompositeForm = function(id, method, action) {
+	this.formComponents = [];
 
+	this.element = document.createElement('form');
+	this.element.id = id;
+	this.element.method = method || 'POST';
+	this.element.action = action || '#';
+};
+
+CompositeForm.prototype.add = function(child) {
+  this.formComponents.push(child);
+  //this.element.appendChild(child.getElement());
+};
+
+CompositeForm.prototype.remove = function(child) {
+  for (var i = 0; i < this.formComponents.length; i++) {
+    if (this.formComponents[i] === child) {
+      this.formComponents.splice(i,1);
+       break;
+    }
+  }
+};
+
+
+
+var el = new CompositeForm(1);
+el.add('foo');
+el.add('bar');
+el.remove('foo');
+console.log(el);
