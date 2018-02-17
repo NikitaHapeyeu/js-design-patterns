ca1a3ac0dafcb2aa91b9da47e11cac77f9174d77
Nikita Hapeyeu <Lard@tut.by>
2018-02-17 20:17:12 +0300

    BicycleShop class improved

--- Factory Pattern/index.js
+++ Factory Pattern/index.js
@@ -1,12 +1,22 @@
-// BicycleShop class
-var BicycleShop = function() {
+// BicycleShop class (improved)
 
-};
+var BicycleShop = function() {};
 
 BicycleShop.prototype = {
 	sellBicycle: function(model) {
-		var bicycle;
+		var bicycle = (new BicycleFactory()).createBicycle(model);
+	
+		return bicycle;
+	}
+};
 
+// BicycleFactory namespace
+
+var BicycleFactory = function() {};
+
+BicycleFactory.prototype = {
+	createBicycle: function(model) {
+		var bicycle;
 		switch(model) {
 			case 'The Speedster': 
 				bicycle = new Speedster();
@@ -14,20 +24,21 @@ BicycleShop.prototype = {
 			case 'The Lowrider':
 				bicycle = new Lowrider();
 				break;
+			case 'Flatlander':
+				bicycle = new Flatlander();
+				break;
 			case 'The Comfort Cruiser':
 			default:
 				bicycle = new ComfortCruiser();
-		}
-		
+		};
+		console.log('bicycle created!');
 		return bicycle;
 	}
 };
 
-var Speedster = function() {
-	console.log('Speedster created!');
-}
-
-
+//constructor 'Speedster'
+var Speedster = function() {};
+ 
 
 var myBicycle = new BicycleShop();
 console.log(myBicycle.sellBicycle('The Speedster'));
