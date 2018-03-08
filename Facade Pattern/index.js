ad63684a05c0f501f6641524fa47eac25229dfad
Nikita Hapeyeu <Lard.tut.by@gmail.com>
2018-03-08 23:30:59 +0300

    set private behavior

--- Facade Pattern/index.js
+++ Facade Pattern/index.js
@@ -1,8 +1,8 @@
 const module = (function() {
     let _private = {
-        i: 5;
+        i: 5,
         get: function() {
-            console.log('current value' + this.i);
+            console.log('current value: ' + this.i);
         },
         set: function(value) {
             this.i = value;
@@ -14,5 +14,19 @@ const module = (function() {
             console.log('jumping');
         }
     };
-    
+    return {
+        facade: function(args) {
+            _private.set(args.val);
+            _private.get();
+            if (args.run) {
+                _private.run();
+            };
+            if (args.jump) {
+                _private.jump();
+            }
+        }
+    }
 }());
+
+module.facade({run: true, val: 42});
+module.facade({jump: true, val: 91});
